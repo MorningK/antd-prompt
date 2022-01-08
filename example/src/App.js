c96743c0396a96c2dec35084c4c17af64fc2c4ab
Kui Chen <chenkui7@foxmail.com>
2022-01-08 19:00:50 +0800

    update example

--- example/src/App.js
+++ example/src/App.js
@@ -1,25 +1,40 @@
+import { Input } from "antd";
+import { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
+import { Prompt } from "@five-show/antd-prompt";
+
+const promptProps = {
+  title: 'Prompt',
+  label: '确认密码',
+  initialValue: 'qwer',
+  children: <Input />,
+};
 
 function App() {
+  const [visible, setVisible] = useState(false);
+  const handleShowPromptComponent = () => {
+    setVisible(true);
+  };
+  const handleShowPromptFunction = async () => {
+    try {
+      const value = await Prompt.prompt({
+        ...promptProps
+      });
+      console.log('prompt value is ', value);
+    } catch (e) {
+      console.log('prompt cancel', e);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={handleShowPromptComponent}>using as component</button>
+        <button onClick={handleShowPromptFunction}>using as function</button>
       </header>
+      <Prompt visible={visible} {...promptProps} onOk={(value) => console.log('prompt value is ', value)} onCancel={() => setVisible(false)} />
     </div>
   );
-}
+};
 
 export default App;
