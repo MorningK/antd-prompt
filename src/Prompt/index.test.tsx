a02515c0e7cc0ffba75ed7729a2354653f5f3b9e
Kui Chen <chenkui7@foxmail.com>
2022-01-21 01:45:23 +0800

    add prompt method tests

--- src/Prompt/index.test.tsx
+++ src/Prompt/index.test.tsx
@@ -9,17 +9,18 @@ import {
 } from '@testing-library/react';
 import Prompt from './index';
 
-test('test Prompt', async () => {
-  const testId = 'test-input';
-  const promptProps = {
-    visible: false,
-    title: 'Prompt',
-    label: 'Price',
-    initialValue: '99',
-    children: <Input data-testid={testId} />,
-  };
-  const handleOk = jest.fn((val) => val);
-  const handleCancel = jest.fn();
+const testId = 'test-input';
+const promptProps = {
+  visible: false,
+  title: 'Prompt',
+  label: 'Price',
+  initialValue: '99',
+  children: <Input data-testid={testId} />,
+};
+const handleOk = jest.fn((val) => val);
+const handleCancel = jest.fn();
+
+test('test Prompt Component', async () => {
   const { rerender } = render(
     <Prompt {...promptProps} onOk={handleOk} onCancel={handleCancel} />
   );
@@ -43,8 +44,10 @@ test('test Prompt', async () => {
   await waitFor(() => expect(input).toHaveValue('199'));
 
   // click cancel button
-  fireEvent.click(cancelBtn);
-  expect(handleCancel).toBeCalled();
+  act(() => {
+    fireEvent.click(cancelBtn);
+  });
+  await waitFor(() => expect(handleCancel).toBeCalled());
 
   // click ok button and return value should be 199
   act(() => {
@@ -59,4 +62,56 @@ test('test Prompt', async () => {
   promptProps.visible = false;
   rerender(<Prompt {...promptProps} onOk={handleOk} onCancel={handleCancel} />);
   expect(screen.queryByTestId(testId)).toBe(null);
+});
+
+test('test Prompt method when ok', async () => {
+  Prompt.prompt({
+    ...promptProps,
+  })
+    .then(handleOk)
+    .catch(handleCancel);
+  // Prompt become visible
+  const input = screen.getByTestId(testId);
+  const okBtn = screen.getByText('OK');
+  expect(input).toBeVisible();
+  expect(input).toHaveValue('99');
+  // change input value to 199
+  act(() => {
+    fireEvent.change(input, { target: { value: '199' } });
+  });
+  await waitFor(() => expect(input).toHaveValue('199'));
+
+  // click ok button and return value should be 199
+  act(() => {
+    fireEvent.click(okBtn);
+  });
+  await waitFor(() => {
+    expect(handleOk).toBeCalled();
+    expect(handleOk.mock.results[0].value).toBe('199');
+    // Prompt is invisible
+    expect(screen.queryByTestId(testId)).toBe(null);
+  });
+});
+
+test('test Prompt method when cancel', async () => {
+  Prompt.prompt({
+    ...promptProps,
+  })
+    .then(handleOk)
+    .catch(handleCancel);
+  // Prompt is visible
+  const input = screen.getByTestId(testId);
+  const cancelBtn = screen.getByText('Cancel');
+  expect(input).toBeVisible();
+  expect(input).toHaveValue('99');
+
+  // click cancel button
+  act(() => {
+    fireEvent.click(cancelBtn);
+  });
+  await waitFor(() => {
+    expect(handleCancel).toBeCalled();
+    // Prompt is invisible
+    expect(screen.queryByTestId(testId)).toBe(null);
+  });
 });
