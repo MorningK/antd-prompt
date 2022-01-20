feed9bb3559b905be660531b8281c5dd94dcf3b8
Kui Chen <chenkui7@foxmail.com>
2022-01-21 01:22:11 +0800

    add tests

--- src/Prompt/index.test.tsx
+++ src/Prompt/index.test.tsx
@@ -1,40 +1,62 @@
 import { Input } from 'antd';
 import React from 'react';
-import { fireEvent, render, screen, act } from '@testing-library/react';
+import {
+  fireEvent,
+  render,
+  screen,
+  act,
+  waitFor,
+} from '@testing-library/react';
 import Prompt from './index';
 
-test('test Prompt', () => {
+test('test Prompt', async () => {
+  const testId = 'test-input';
   const promptProps = {
     visible: false,
     title: 'Prompt',
     label: 'Price',
     initialValue: '99',
-    children: <Input data-testid="test-input" />,
+    children: <Input data-testid={testId} />,
   };
-  const handleOk = jest.fn();
+  const handleOk = jest.fn((val) => val);
   const handleCancel = jest.fn();
   const { rerender } = render(
     <Prompt {...promptProps} onOk={handleOk} onCancel={handleCancel} />
   );
+  // the value of visible prop is false so Prompt is invisible
+  expect(screen.queryByTestId(testId)).toBe(null);
 
-  expect(screen.queryByTestId('test-input')).toBe(null);
-
+  // set visible to true and rerender
   promptProps.visible = true;
   rerender(<Prompt {...promptProps} onOk={handleOk} onCancel={handleCancel} />);
-  const input = screen.getByTestId('test-input');
+  // Prompt become visible
+  const input = screen.getByTestId(testId);
   const cancelBtn = screen.getByText('Cancel');
   const okBtn = screen.getByText('OK');
   expect(input).toBeVisible();
   expect(input).toHaveValue('99');
 
-  // act(() => {
-  //   fireEvent.change(input, {target: {value: '199'}});
-  // });
-  // expect(input).toHaveValue('199');
+  // change input value to 199
+  act(() => {
+    fireEvent.change(input, { target: { value: '199' } });
+  });
+  await waitFor(() => expect(input).toHaveValue('199'));
 
+  // click cancel button
   fireEvent.click(cancelBtn);
   expect(handleCancel).toBeCalled();
 
-  // fireEvent.click(okBtn);
-  // expect(handleOk).toBeCalled();
+  // click ok button and return value should be 199
+  act(() => {
+    fireEvent.click(okBtn);
+  });
+  await waitFor(() => {
+    expect(handleOk).toBeCalled();
+    expect(handleOk.mock.results[0].value).toBe('199');
+  });
+
+  // set visible to false and rerender
+  promptProps.visible = false;
+  rerender(<Prompt {...promptProps} onOk={handleOk} onCancel={handleCancel} />);
+  expect(screen.queryByTestId(testId)).toBe(null);
 });
